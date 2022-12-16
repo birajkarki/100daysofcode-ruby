bf0fbc5d76a5a957c4b4efc6eb2f0cc3edf3a883
Biraj <birajkarki9849@gmail.com>
2022-12-17 01:11:45 +0545

    commit

--- Day1/Hello.rb
+++ Day1/Hello.rb
@@ -1,2 +1,42 @@
 puts "Hello World!" #adds a new (blank) line
 print "Hello World!"
+
+
+my_num =     25
+my_boolean =    true
+my_string =    "Ruby"
+
+puts my_num
+puts my_boolean
+puts my_string
+
+puts "hello" 
+ 
+
+my_num = 2 + 3
+ puts my_num
+# comment 
+# I am a single line comment.
+
+=begin
+I am a multi line comment. 
+I can take as many lines as needed.
+=end
+
+a = "I am Biraj Karki".length
+puts a
+
+name = "biraj".reverse
+puts name
+
+name = "biraj".upcase
+name = "biraj".downcase
+puts name
+
+name = "biraj" #this is my name
+puts name
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 /17
+
