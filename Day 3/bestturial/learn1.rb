e931e47fd845c5926688219b1048cbad09c64b29
Biraj <birajkarki9849@gmail.com>
2022-12-19 23:36:39 +0545

    c

--- Day 3/bestturial/learn1.rb
+++ Day 3/bestturial/learn1.rb
@@ -1,3 +1,16 @@
-puts "Hello World"
-print "Hello World"
-p "Hello Worlds"
+# puts "Hello World"
+# print "Hello World"
+# p "Hello Worlds"
+
+# Debugging
+=begin
+
+ Syntax Errors
+ Runtime Errors
+ Semantic Errors
+=end
+
+# Comments
+
+message = "Hello Biraj"
+puts message
