a6cf8fdf2a1c1635acfd4a7c08f518d8c7cbe220
Biraj <birajkarki9849@gmail.com>
2022-12-24 23:20:23 +0545

    commit

--- Day7/game.rb
+++ Day7/game.rb
@@ -3,7 +3,7 @@ require 'ruby2d'
 set background: 'green'
 set title: 'Reaction Game'
 
-message = Text.new('Click to begin', x: 20, y: 20)
+message = Text.new('Click to begin', x: 25, y: 25)
 game_started = false
 square = nil
 start_time = nil
@@ -13,7 +13,7 @@ on :mouse_down do |event|
   if game_started
     if square.contains?(event.x, event.y)
       duration = ((Time.now - start_time) * 1000).round
-      message = Text.new("Well done! You took: #{duration} milliseconds. Click to begin", x: 20, y: 20)
+      message = Text.new("Well done! You took: #{duration} milliseconds. Click to begin", x: 25, y: 25)
       square.remove
       game_started = false
     end
