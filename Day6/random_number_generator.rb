53008a01d851605b18d85e62e8e4d945a1053f1a
Biraj <birajkarki9849@gmail.com>
2022-12-24 23:34:21 +0545

    commit

---
+++ Day6/random_number_generator.rb
@@ -0,0 +1,29 @@
+# Generate a random number between 1 and 100
+secret_number = rand(1..100)
+
+# Prompt the user to guess the number
+puts "Guess a number between 1 and 100:"
+
+# Read the user's guess
+guess = gets.chomp.to_i
+
+# Keep track of the number of guesses
+num_guesses = 1
+
+# Loop until the user guesses the secret number
+until guess == secret_number
+  if guess < secret_number
+    puts "Your guess is too low. Try again:"
+  else
+    puts "Your guess is too high. Try again:"
+  end
+
+  # Read the user's guess
+  guess = gets.chomp.to_i
+
+  # Increment the number of guesses
+  num_guesses += 1
+end
+
+# The user has guessed the secret number
+puts "You guessed the number in #{num_guesses} tries!"
